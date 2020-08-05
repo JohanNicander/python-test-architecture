7b9a8a1372b039733553d594477994c071066b56
Johan Nicander <johan.nicander@ibm.com>
2020-08-05 17:26:52 +0200

    Updated the structure to match qiskit

--- main_module/_unittester.py
+++ main_module/_unittester.py
@@ -3,35 +3,36 @@ import os
 
 __all__ = ['UnitTester']
 
-
 class UnitTester:
 
     def __init__(self, module_name):
         self.module_name = module_name
 
-    def __call__(self, color=False):
+    def __call__(self):
 
         from stestr.commands import run_command
         # import warnings
 
         module = sys.modules[self.module_name]
-        module_path = os.path.abspath(module.__path__[0])
-        cwd = os.getcwd()
-        os.chdir(module_path)
+        base_module = sys.modules["main_module"]
+        base_module_path = os.path.abspath(base_module.__path__[0])
+        module_path = os.path.relpath(module.__path__[0], base_module_path)
+        test_path = os.path.join(base_module_path, "tests", module_path)
+        # cwd = os.getcwd()
+        # os.chdir(test_path)
 
         stestr_args = {
-            "test_path": module_path,
-            "top_dir": module_path,
+            "test_path": test_path,
+            "top_dir": test_path,
             "group_regex": r"([^\.]*\.)*",
-            "color": color,
         }
 
         try:
             code = run_command(**stestr_args)
         except SystemExit as exc:
             code = exc.code
-        finally:
-            os.chdir(cwd)
+        # finally:
+        #     os.chdir(cwd)
 
         return code == 0
         # return 0
