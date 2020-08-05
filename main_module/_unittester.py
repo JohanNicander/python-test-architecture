2418f861cb46c3fccaa21be94ee92c5862985a15
Johan Nicander <johan.nicander@ibm.com>
2020-08-05 19:54:07 +0200

    Fixed typo, removed dead code and normalized path

--- main_module/_unittester.py
+++ main_module/_unittester.py
@@ -11,16 +11,14 @@ class UnitTester:
     def __call__(self, *args, **kwargs):
 
         import unittest
-        from unittest import main, defaultTestLoader
-        # import warnings
 
         module = sys.modules[self.module_name]
         base_module = sys.modules["main_module"]
         base_module_path = os.path.abspath(base_module.__path__[0])
         module_path = os.path.relpath(module.__path__[0], base_module_path)
-        test_path = os.path.join(base_module_path, "tests", module_path)
+        test_path = os.path.normpath(os.path.join(base_module_path, "tests", module_path))
 
-        tests = defaultTestLoader.discover(test_path)
+        tests = unittest.defaultTestLoader.discover(test_path)
         textTestRunner = unittest.TextTestRunner(*args, **kwargs)
         textTestResult = textTestRunner.run(tests)
 
